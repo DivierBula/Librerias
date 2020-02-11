98c4df65983dc607110b5f073dcbda1c95b0bbf0
DivierBula <60910413+DivierBula@users.noreply.github.com>
2020-02-10 23:38:52 -0500

    JWT 2.0

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using CT.Common.Models;
+using System.Net;
+using System.Web.Http;
+
+namespace WebApiBiblioteca.Controllers
+{
+    [AllowAnonymous]
+    [RoutePrefix("Api/login")]
+    public class LoginController : ApiController
+    {
+        [HttpGet]
+        [Route("Ping")]
+        public IHttpActionResult Ping()
+        {
+            return Ok(true);
+        }
+
+        [HttpPost]
+        [Route("authenticate")]
+        public IHttpActionResult Authenticate(LoginRequestDTO login)
+        {
+            if (login == null)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var token = TokenGenerator.GenerateTokenJwt(login.Username);
+            return Ok(token);
+
+            //Para no autorizar el Ingreso
+            //return Unauthorized();
+
+        }
+
+    }
+}
---
+++ Controllers/MecanicoController.cs
@@ -0,0 +1,81 @@
+﻿using GeneradorEnrutador.BusinessLogic;
+using System.Collections.Generic;
+using System.Web.Http;
+using CT.Common.Models;
+using CT.Common.Utilities;
+using Newtonsoft.Json;
+
+namespace WebApiBiblioteca.Controllers
+{
+    [Authorize]
+    //[AllowAnonymous]
+    [RoutePrefix("api/Mecanico")]
+    public class MecanicoController : ApiController
+    {
+        
+        [HttpGet]
+        [Route("ConsultarMecanicos")]
+        public IHttpActionResult Consultar_Mecanicos()
+        {
+            var IOperacion = GeneradorOperacionBL.InstanciarClase(Enumeradores.clases.MecanicoBL.ToStringAttribute());
+            RespuestaDTO response = IOperacion.EjecutarOperacion(Enumeradores.clases.MecanicoBL.ToStringAttribute(), System.Reflection.MethodBase.GetCurrentMethod().Name, null);
+
+            if (response.Exito && response.Mecanicos.IsNullOrEmpty())
+            {
+                return Ok(new RespuestaDTO()
+                {
+                    Exito = true,
+                    Mensaje = "No se encontro Información",
+                });
+            }
+            else
+                return Ok(response);
+        }
+
+        [HttpGet]
+        [Route("ConsultarMecanicosXid")]
+        public IHttpActionResult Consultar_Mecanicos_X_id(string id)
+        {
+            var IOperacion = GeneradorOperacionBL.InstanciarClase(Enumeradores.clases.MecanicoBL.ToStringAttribute());
+            RespuestaDTO response = IOperacion.EjecutarOperacion(Enumeradores.clases.MecanicoBL.ToStringAttribute(), System.Reflection.MethodBase.GetCurrentMethod().Name, new List<string> { id });
+
+            if (response.Exito && response.Mecanicos.IsNullOrEmpty())
+            {
+                return Ok(new RespuestaDTO()
+                {
+                    Exito = true,
+                    Mensaje = "No se encontro Información",
+                });
+            }
+            else
+                return Ok(response);
+        }
+
+        [HttpPost]
+        [Route("Crear_Mecanico")]
+        public IHttpActionResult Crear_Mecanico(MecanicoDTO Mecanico)
+        {
+            var IOperacion = GeneradorOperacionBL.InstanciarClase(Enumeradores.clases.MecanicoBL.ToStringAttribute());
+            RespuestaDTO response = IOperacion.EjecutarOperacion(Enumeradores.clases.MecanicoBL.ToStringAttribute(), System.Reflection.MethodBase.GetCurrentMethod().Name, new List<string> { JsonConvert.SerializeObject(Mecanico) });
+            return Ok(response);
+        }
+
+        [HttpPost]
+        [Route("Actualizar_Mecanico")]
+        public IHttpActionResult Actualizar_Mecanico(MecanicoDTO Mecanico)
+        {
+            var IOperacion = GeneradorOperacionBL.InstanciarClase(Enumeradores.clases.MecanicoBL.ToStringAttribute());
+            RespuestaDTO response = IOperacion.EjecutarOperacion(Enumeradores.clases.MecanicoBL.ToStringAttribute(), System.Reflection.MethodBase.GetCurrentMethod().Name, new List<string> { JsonConvert.SerializeObject(Mecanico) });
+            return Ok(response);
+        }
+
+        [HttpDelete]
+        [Route("Eliminar_Mecanico")]
+        public IHttpActionResult Eliminar_Mecanico(string id)
+        {
+            var IOperacion = GeneradorOperacionBL.InstanciarClase(Enumeradores.clases.MecanicoBL.ToStringAttribute());
+            RespuestaDTO response = IOperacion.EjecutarOperacion(Enumeradores.clases.MecanicoBL.ToStringAttribute(), System.Reflection.MethodBase.GetCurrentMethod().Name, new List<string> { id });
+            return Ok(response);
+        }
+    }
+}
