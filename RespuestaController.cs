125aa40716922add0e66caefa481b5e0e25670a3
DivierBula <60910413+DivierBula@users.noreply.github.com>
2020-03-17 07:15:06 -0500

    Add files via upload

---
+++ RespuestaController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using ModelCobertura;
+using System.Web;
+using System;
+
+namespace ConsultaCobertura.Controllers
+{
+    public class RespuestaController : Controller
+    {
+        // GET: Respuesta
+        public ActionResult Respuesta()
+        {
+            string valorCookie = System.Web.HttpContext.Current.Session["sessionCookie"].ToString();
+            ViewBag.valorCookie = valorCookie; //(modelo.RespuestaDTO != null && modelo.RespuestaDTO.Descripcion != null ? modelo.RespuestaDTO.Descripcion : "");
+            return View("Respuesta");
+        }               
+        
+        public ActionResult consultarCookie()
+        {
+            string valor = Request.Cookies["respuestaCobertura"].Value;
+            return Json(new { responseText = "OK", valorCookie = valor});
+        } 
+    }
+}
